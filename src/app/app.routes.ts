5f078ac76ada6d5bcd2e76b3757453a09dc3d7da
Sávio Marques <saviomarquessouza@gmail.com>
2024-12-28 15:30:59 -0300

    Create Sing Up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SingupComponent } from './pages/singup/singup.component';
 
 export const routes: Routes = [
     {
     path: "login",
     component: LoginComponent
+    },
+    {
+    path: "singup",
+    component: SingupComponent
     }
 ];
--- src/app/pages/singup/singup.component.ts
+++ src/app/pages/singup/singup.component.ts
@@ -6,13 +6,15 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm{
+interface SingupForm{
+  name: FormControl,
   email: FormControl,
   password: FormControl,
+  passwordConfirm: FormControl
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-singup',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
@@ -22,25 +24,28 @@ interface LoginForm{
   providers: [
     LoginService
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './singup.component.html',
+  styleUrl: './singup.component.scss'
 })
-export class LoginComponent {
-  loginForm !: FormGroup<LoginForm>;
+export class SingupComponent {
+  singupForm!: FormGroup<SingupForm>;
   
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastService: ToastrService 
   ) {
-    this.loginForm = new FormGroup({
+    this.singupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
+
     });
   }
 
   submit(){
-    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+    this.loginService.login(this.singupForm.value.email, this.singupForm.value.password).subscribe({
       next: () => this.toastService.success("Login feito com sucesso!"),
       error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
     })
@@ -48,6 +53,6 @@ export class LoginComponent {
 
 
   navigate() {
-    this.router.navigate(["singup"])
+    this.router.navigate(["login"])
   }
 }
